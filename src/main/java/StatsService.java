a19e7b8201b42319eec81a3791de5e16acdfce68
dps <dobro.polya@yandex.ru>
2023-02-12 23:18:21 +0300

    Initial commit

---
+++ src/main/java/StatsService.java
@@ -0,0 +1,74 @@
+public class StatsService {
+    public int minSales(long[] sales) {//Метод ниже считает номер месяца минимальных продаж
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public int maxSales(long[] sales) {//Метод ниже считает номер месяца максимальных  продаж
+        int maxMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return maxMonth + 1;
+    }
+
+    public long sumSales(long[] sales) {//Метод ниже считает сумму  продаж
+        long sum = 0;
+        for (long num : sales) {
+            // суммирование каждого элемента массива
+            sum = sum + num;
+        }
+        return sum;
+    }
+
+    public long avgSales(long[] sales) {//Метод ниже считает среднюю сумму  продаж
+        long sum = 0;
+        for (long num : sales) {
+            // суммирование каждого элемента массива
+            sum = sum + num;
+        }
+        return sum / sales.length;
+    }
+
+    public long avgLowerSales(long[] sales) {
+        long sum = 0;
+        long cnt = 0;
+        for (long num : sales) {
+            sum = sum + num;
+        }
+        long avgSum= sum / sales.length;
+        for (int i=0 ; i<sales.length;i++) {
+            if (avgSum < sales[i]) {
+                cnt = cnt +1;
+            }
+        }
+        return cnt;
+    }
+    public long avgHigherSales(long[] sales) {
+        long sum = 0;
+        long cnt = 0;
+        for (long num : sales) {
+            sum = sum + num;
+        }
+        long avgSum= sum / sales.length;
+        for (int i=0 ; i<sales.length;i++) {
+            if (avgSum >sales[i]) {
+                cnt = cnt +1;
+            }
+        }
+        return cnt;
+    }
+}
---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,87 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatsServiceTest {
+    @org.junit.jupiter.api.Test
+    public void StatServiceMinMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 9;
+        // вызываем целевой метод:
+        int actual = service.minSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void StatServiceMaxMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 8;
+        // вызываем целевой метод:
+        int actual = service.maxSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void StatServiceSumMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 180;
+        // вызываем целевой метод:
+        long actual = service.sumSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void StatServiceAvgMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 15;
+        // вызываем целевой метод:
+        long actual = service.avgSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void StatServiceLowerAvgMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        // вызываем целевой метод:
+        long actual = service.avgLowerSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void StatServiceHigherAvgMonthSales() {
+        StatsService service = new StatsService();
+
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expected = 5;
+        // вызываем целевой метод:
+        long actual = service.avgHigherSales(sales);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+}
